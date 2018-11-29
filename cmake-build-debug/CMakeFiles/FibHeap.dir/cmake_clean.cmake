ef33eb12ee0075ece2c87c35823c38ae78f40823
tsinin <andrew.tsinin@mail.ru>
2018-11-29 22:05:37 +0300

    init commit

---
+++ cmake-build-debug/CMakeFiles/FibHeap.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FibHeap.dir/main.cpp.obj"
+  "FibHeap.pdb"
+  "FibHeap.exe"
+  "FibHeap.exe.manifest"
+  "libFibHeap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FibHeap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
